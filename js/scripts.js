eca509d870bc90c549be596cc2bddc9af0832172
Dana Weiss and Kat Gifford <kat@>
2017-03-29 14:28:51 -0700

    add indexOf method to evaluate characters by vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // // Business Logic //
 var vowels =["a","A","e","E","i" ,"I","o","O","u","U"];
 
-
+// vowels.indexOf("a");
 
 // Ux //
 $(document).ready(function() {
@@ -11,22 +11,22 @@ $(document).ready(function() {
     var input = ($("input#uxInput").val()).split(" ");
     console.log(input);
     var ay = [];
-    // var syl = ("|"+input+"|").split(/[aeiou]/i);
 
     for(var i=0; i<input.length; i++){
       input[i][0] = input[i][0].charAt(0);
       console.log(input[i].charAt(0));
-
-      
-      // if (input[i].charAt(0)) === ;
-      function vowelRegEx(char)
-      {
-        if (char.length == 1)
-        {
-          return /[aeiou]/.test(char);
-        }
-      }
+       console.log(vowels.indexOf(input[i].charAt(0)));
     }
+
+
+    //   function vowelRegEx(char)
+    //   {
+    //     if (char.length == 1)
+    //     {
+    //       return /[aeiou]/.test(char);
+    //     }
+    //   }
+    // }
 
       // var char0 = (input[i].charAt(0));
     //   console.log(input[i]);
