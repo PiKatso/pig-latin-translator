1b626b92d70d2b292627d126ad53bc35b88e45c3
Dana Weiss and Kat Gifford <dana@>
2017-03-29 14:11:46 -0700

    add for loop that allowed us to apply charAt and return 1st characters in each string within input array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
 // // Business Logic //
-// var vowels =["a","A","e","E","i" ,"I","o","O","u","U"];
-//
+var vowels =["a","A","e","E","i" ,"I","o","O","u","U"];
+
 
 
 // Ux //
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
-    
+
     var input = ($("input#uxInput").val()).split(" ");
-    console.log($("input#uxInput").val());
     console.log(input);
+    var ay = [];
+    // var syl = ("|"+input+"|").split(/[aeiou]/i);
+
+    for(var i=0; i<input.length; i++){
+      input[i][0] = input[i][0].charAt(0);
+      console.log(input[i].charAt(0));
+
+      
+      // if (input[i].charAt(0)) === ;
+      function vowelRegEx(char)
+      {
+        if (char.length == 1)
+        {
+          return /[aeiou]/.test(char);
+        }
+      }
+    }
+
+      // var char0 = (input[i].charAt(0));
+    //   console.log(input[i]);
+    // } return ;
+
+
   });
 });
